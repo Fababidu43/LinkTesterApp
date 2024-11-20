a4ee41af9d54b5bda3c7b6422aa01f991a37d3a3
Fababidu43 <fabian.measson.pro@gmail.com>
2024-11-20 16:34:36 +0100

    Dynamiser setup.iss avec la version du tag et ajouter une étape de vérification

--- LinkTesterApp.py
+++ LinkTesterApp.py
@@ -199,7 +199,7 @@ class LinkTesterApp:
             messagebox.showerror("Erreur", "Impossible de vérifier les mises à jour.")
 
 def get_latest_release(repo_owner, repo_name):
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
--- setup.iss
+++ setup.iss
@@ -1,7 +1,7 @@
 ; Script Inno Setup Corrigé
 [Setup]
 AppName=LinkTesterApp
-AppVersion=1.0.7
+AppVersion=1.0.8
 DefaultDirName={pf}\LinkTesterApp
 DefaultGroupName=LinkTesterApp
 OutputBaseFilename=setup_LinkTesterApp
